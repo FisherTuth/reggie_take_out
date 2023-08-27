60f39d3b3c846e3e6727d327c063cc8ed3354735
FisherTuth <975651643@qq.com>
2023-08-27 18:15:42 +0800

    登录功能

---
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,57 @@
+package com.itheima.reggie.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(filterName = "LoginCheckFilter",urlPatterns = "/*")
+public class LoginCheckFilter implements Filter {
+    public static final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        String requestUri = request.getRequestURI();
+
+        log.info("拦截到请求：{}",requestUri);
+
+        String[]urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**",
+        };
+
+        if(check(urls,requestUri)){
+            log.info("本次请求：{}不需要处理",requestUri);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        if(request.getSession().getAttribute("employee")!=null){
+            log.info("用户已登录，id为{}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+            return;
+        }
+        log.info("用户未登录");
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+    }
+
+    private boolean check(String[]urls,String Uri){
+
+        for(String url : urls){
+            if (pathMatcher.match(url,Uri))return true;
+        }
+        return false;
+    }
+}
